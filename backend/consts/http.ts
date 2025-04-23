2331b1a0adfc6440b257287e4de5ddf844ff7fc3
breslavsky <breslavsky.anton@gmail.com>
2025-04-24 00:39:35 +0200

    fix: content headers

--- backend/consts/http.ts
+++ backend/consts/http.ts
@@ -4,7 +4,6 @@ export const SCP_HEADERS = {
 };
 
 export const NO_CACHE_HEADERS = {
-  "Content-Type": "text/html; charset=utf-8",
   "Cache-Control": "no-store, no-cache, must-revalidate, proxy-revalidate",
   Pragma: "no-cache",
   Expires: "0",
