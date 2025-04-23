7c89d7df0a7aeaaed2f338f1c82904dd6cffcab8
Stanislav Lapshin <sm.lapshin@gmail.com>
2025-04-23 18:12:24 +0300

    revert: "feat(clickhouse): add migrations"
    This reverts commit 0162349fa8bf3a13814c71d9b9e052ec1d79cc8e.

--- misc/clickhouse/migrations.sql
+++ misc/clickhouse/migrations.sql
@@ -1,29 +1,5 @@
--- [ ] add url to `user_balance_refills`
-ALTER TABLE user_balance_refills
-ADD COLUMN url String;
--- [ ] update `pipeline_metrics_finished` 
-ALTER TABLE pipeline_metrics_finished
-ADD COLUMN pipelineName String;
-ALTER TABLE pipeline_metrics_finished
-ADD COLUMN durationSecs UInt16;
-ALTER TABLE pipeline_metrics_finished
-ADD COLUMN errorsCount UInt8;
-ALTER TABLE pipeline_metrics_finished DROP COLUMN fromStart;
---
-DROP TABLE IF EXISTS pipeline_metrics_finished_consumer;
-DROP VIEW IF EXISTS pipeline_metrics_finished_mv;
-CREATE TABLE pipeline_metrics_finished_consumer (
-    launch String,
-    finishedAt DateTime,
-    pipelineName String,
-    durationSecs UInt16,
-    errorsCount UInt8
-) ENGINE = Kafka(
-    'kafka:9092',
-    'pipeline.metrics.finished',
-    'piper-clickhouse',
-    'JSONEachRow'
-);
-CREATE MATERIALIZED VIEW pipeline_metrics_finished_mv TO pipeline_metrics_finished AS
-SELECT *
-FROM pipeline_metrics_finished_consumer;
+- [ ] add url to `user_balance_refills`
+
+```sql
+ALTER TABLE user_balance_refills ADD COLUMN url String;
+```
