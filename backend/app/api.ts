ba09cec97ea5e30ae756180a243fb0507c67d202
breslavsky <breslavsky.anton@gmail.com>
2025-06-07 16:00:15 +0200

    refactor: move CORS to traefik

--- backend/app/api.ts
+++ backend/app/api.ts
@@ -1,18 +1,3 @@
 import api from "core-kit/packages/server";
 
-api.use((req, res, next) => {
-  res.set({
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-    "Access-Control-Allow-Headers": "content-type, api-token",
-    "Access-Control-Allow-Credentials": "true",
-  });
-
-  if (req.method === "OPTIONS") {
-    res.status(204).send();
-    return;
-  }
-
-  next();
-});
 export default api;
